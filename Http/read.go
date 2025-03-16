d9fd98b61e24f712aca79586fe6a34aaa159b693
chris.liu <5274913+chris-liu-zh@user.noreply.gitee.com>
2025-03-17 01:09:37 +0800

    delete ReadAll

--- Http/read.go
+++ Http/read.go
@@ -2,30 +2,7 @@
  * @Author: Chris
  * @Date: 2025-03-17 00:14:34
  * @LastEditors: Chris
- * @LastEditTime: 2025-03-17 00:58:40
+ * @LastEditTime: 2025-03-17 01:08:54
  * @Description: 请填写简介
  */
 package Http
-
-import (
-	"encoding/json"
-	"io"
-	"net/http"
-	"reflect"
-)
-
-func ReadAll(r *http.Request, v any) error {
-	// 检查 v 是否为指针
-	rv := reflect.ValueOf(v)
-	if rv.Kind() != reflect.Pointer || rv.IsNil() {
-		return &json.InvalidUnmarshalError{Type: reflect.TypeOf(v)}
-	}
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		return err
-	}
-	if err = json.Unmarshal(body, v); err != nil {
-		return err
-	}
-	return nil
-}
