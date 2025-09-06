371d2b517178afd90f0cba9dfc826c7e41fb8fbb
chris.liu <5274913+chris-liu-zh@user.noreply.gitee.com>
2025-09-06 18:03:13 +0800

    去除缓存持久化

--- test/cache_test.go
+++ test/cache_test.go
@@ -16,17 +16,14 @@ func Test_Cache(t *testing.T) {
 		return
 	}
 	start := time.Now()
-	count := int64(0)
 	for i := range 10000 {
 		key := fmt.Sprintf("key%05d", i)
 		val := fmt.Sprintf("value%05d", i)
 		kv.Set(key, val, 60*time.Second)
-		count += int64(len(key)) + int64(len(val)) + 8
 	}
 
 	fmt.Println("set cost:", time.Since(start).String())
 	kv.Count()
-	fmt.Println("count:", count)
 	fmt.Println("end1111")
 	time.Sleep(5 * time.Second)
 }
